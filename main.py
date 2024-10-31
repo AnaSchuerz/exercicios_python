5f979bf002822e698e04b69cc9208fc2852ad8e6
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2024-10-31 14:34:34 -0300

    new image

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-1, 1, 1000)
-y = np.sin(x)
+z = np.cos(x)
 
-plt.plot(x,y)
+plt.plot(x, z)
+plt.savefig("plot3.png", dpi=300)
 plt.show()
